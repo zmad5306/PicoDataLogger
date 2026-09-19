8fa697669ab58acca798b095ddb51aae3f1f6a49
Zach Maddox <zmad5306@protonmail.com>
2026-09-18 20:10:20 -0400

    Add USB serial diagnostics

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,41 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_rp::bind_interrupts;
+use embassy_rp::peripherals::USB;
+use embassy_rp::usb::{Driver, InterruptHandler};
+use embassy_time::{Instant, Timer};
 use panic_halt as _;
+
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+type UsbDriver = Driver<'static, USB>;
+
+#[embassy_executor::task]
+async fn logger_task(driver: UsbDriver) -> ! {
+    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+}
 
 #[embassy_executor::main(
     executor = "embassy_rp::executor::Executor",
     entry = "cortex_m_rt::entry"
 )]
-async fn main(_spawner: Spawner) {
-    let _peripherals = embassy_rp::init(Default::default());
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let driver = Driver::new(p.USB, Irqs);
+
+    spawner.spawn(logger_task(driver).expect("Failed to start logger task"));
+
+    let started_at = Instant::now();
+
+    Timer::after_secs(2).await;
+
+    log::info!("Pico Data Logger v{} starting", env!("CARGO_PKG_VERSION"));
 
     loop {
-        Timer::after(Duration::from_secs(1)).await;
+        log::info!("uptime: {} seconds", started_at.elapsed().as_secs());
+        Timer::after_secs(5).await;
     }
 }
